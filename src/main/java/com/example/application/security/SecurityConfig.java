bc6a37c949b37b11eda72913886813a163d48c06
Cherri07 <sa1177@mci4me.at>
2023-06-25 21:07:19 +0200

    123

---
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.application.security;
+
+
+import com.example.application.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/images/**").permitAll();
+
+        super.configure(http);
+        setLoginView(http, LoginView.class);
+    }
+
+    @Bean
+    //@Override
+    protected UserDetailsService users() {
+        return new InMemoryUserDetailsManager(User.withUsername("user")
+                .password("{noop}userpass")
+                .roles("USER")
+                .build()); //don't do this in production!
+    }
+}
---
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,63 @@
+package com.example.application.views;
+
+import com.example.application.security.SecurityService;
+import com.example.application.views.list.ListView;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+public class MainLayout extends AppLayout {
+
+    private SecurityService securityService;
+
+    public MainLayout(SecurityService securityService) {
+        this.securityService = securityService;
+        createHeader();
+        createDrawer();
+    }
+
+    private void createHeader() {
+
+        H1 logo = new H1("Vaadin CRM");
+        logo.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.MEDIUM
+        );
+
+        Button logoutButton = new Button("Logout", e -> securityService.logout());
+
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logoutButton);
+
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo);
+        header.setWidthFull();
+        header.addClassNames(
+                LumoUtility.Padding.Vertical.NONE,
+                LumoUtility.Padding.Horizontal.MEDIUM
+        );
+
+        addToNavbar(header);
+    }
+
+    private void createDrawer() {
+
+        RouterLink listView = new RouterLink("List", ListView.class);
+        listView.setHighlightCondition(HighlightConditions.sameLocation());
+
+        addToDrawer(new VerticalLayout(
+                listView,
+                new RouterLink("Dashboard", DashboardView.class)
+        ));
+    }
+
+
+
+
+}
